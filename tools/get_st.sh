530775315463ca5f2536aaee2d97215c251cc6f0
theelsni@gmail.com <theelsni@gmail.com>
2023-03-29 13:48:02 +0200

    get_st fix with own patch

--- tools/get_st.sh
+++ tools/get_st.sh
@@ -5,12 +5,12 @@ cd ../..
 
 git clone https://git.suckless.org/st
 
-
 cd st
 wget https://st.suckless.org/patches/scrollback/st-scrollback-ringbuffer-0.8.5.diff
-wget https://st.suckless.org/patches/visualbell/st-visualbell-0.8.4.diff
+cp ../linux_stuff/tools/visualbell.diff .
+
 patch -p1 < st-scrollback-ringbuffer-0.8.5.diff
-patch -p1 < st-visualbell-0.8.4.diff
+patch -p1 < visualbell.diff
 
 echo "all patches applied correctly?"
 echo "if not, match the rejection files and apply manually "
